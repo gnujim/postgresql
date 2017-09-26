663216d179aa20e252c034616c5636a2df8afebc
Mijung Lee <Mijung@Stephens-MacBook-Air.local>
2017-09-26 12:30:10 -0700

    added knex and implemented same lookup function with knex

---
+++ add_person.js
@@ -0,0 +1,19 @@
+const input = process.argv[2];
+const settings = require('./settings'); //settings.json
+const knex = require('knex')({
+  client: 'pg',
+  version: '7.2',
+  connection: {
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
+  }
+});
+
+knex('famous_people').where('first_name', input).asCallback((err, result) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(result);
+});
